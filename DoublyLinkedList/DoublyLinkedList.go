2e54decbb75cc2560706e0bc06eedcf28f6b7cad
sj0n <hariz@proton.me>
2023-11-07 18:51:47 +0800

    add: DoublyLinkedList

---
+++ DoublyLinkedList/DoublyLinkedList.go
@@ -0,0 +1,130 @@
+package doublylinkedlist
+
+import "errors"
+
+type Node[T any] struct {
+	Prev  *Node[T]
+	Next  *Node[T]
+	Value T
+}
+
+type DoublyLinkedList[T any] struct {
+	Head   *Node[T]
+	Tail   *Node[T]
+	Length int
+}
+
+func (dll *DoublyLinkedList[T]) IsEmpty() bool {
+	return dll.Head == nil
+}
+
+func (dll *DoublyLinkedList[T]) Prepend(value T) {
+	node := &Node[T]{Value: value}
+	dll.Length++
+
+	if dll.IsEmpty() {
+		dll.Head = node
+		dll.Tail = node
+		return
+	}
+
+	node.Next = dll.Head
+	dll.Head.Prev = node
+	dll.Head = node
+}
+
+func (dll *DoublyLinkedList[T]) Append(value T) {
+	node := &Node[T]{Value: value}
+	dll.Length++
+
+	if dll.IsEmpty() {
+		dll.Head = node
+		dll.Tail = node
+		return
+	}
+
+	node.Prev = dll.Tail
+	dll.Tail.Next = node
+	dll.Tail = node
+}
+
+func (dll *DoublyLinkedList[T]) GetAt(index int) (*Node[T], interface{}) {
+	currentNode := dll.Head
+
+	if currentNode == nil {
+		return nil, errors.New("head is empty")
+	}
+
+	for i := 0; currentNode != nil && i < index; i++ {
+		currentNode = currentNode.Next
+	}
+
+	return currentNode, nil
+}
+
+func (dll *DoublyLinkedList[T]) InsertAt(value T, index int) interface{} {
+	if index < 0 || index > dll.Length {
+		return errors.New("index is out of bound")
+	}
+
+	if index == dll.Length {
+		dll.Append(value)
+	} else if index == 0 {
+		dll.Prepend(value)
+	} else {
+		currentNode, error := dll.GetAt(index)
+
+		if error != nil {
+			return error
+		}
+
+		dll.Length++
+		node := &Node[T]{Value: value}
+
+		node.Next = currentNode
+		node.Prev = currentNode.Prev
+		currentNode.Prev = node
+
+		if node.Prev != nil {
+			node.Prev.Next = node
+		}
+	}
+	return nil
+}
+
+func (dll *DoublyLinkedList[T]) RemoveAt(index int) (interface{}, interface{}) {
+	if index < 0 || index > dll.Length {
+		return nil, errors.New("index is out of bound")
+	}
+
+	node, error := dll.GetAt(index)
+
+	if error != nil {
+		return nil, error
+	}
+
+	dll.Length--
+
+	if dll.Length == 0 {
+		node := dll.Head
+		dll.Head = nil
+		dll.Tail = nil
+
+		return node.Value, nil
+	}
+
+	if node.Prev != nil && node.Next != nil {
+		node.Prev.Next = node.Next
+		node.Next.Prev = node.Prev
+	}
+
+	if node == dll.Head {
+		dll.Head = node.Next
+	}
+
+	if node == dll.Tail {
+		dll.Tail = node.Prev
+	}
+
+	return node.Value, nil
+}
---
+++ DoublyLinkedList/DoublyLinkedList_test.go
@@ -0,0 +1,94 @@
+package doublylinkedlist
+
+import "testing"
+
+func TestDoublyLinkedList(t *testing.T) {
+	list := DoublyLinkedList[int]{}
+	t.Run("Append()", func(t *testing.T) {
+		list.Append(1)
+		list.Append(2)
+		list.Append(3)
+		t.Run("Length is 3", func(t *testing.T) {
+			if list.Length != 3 {
+				t.Errorf("Expected %d", 3)
+				t.Errorf("Received %d", list.Length)
+			}
+		})
+
+		t.Run("Head value is 1", func(t *testing.T) {
+			if list.Head.Value != 1 {
+				t.Errorf("Expected %d", 1)
+				t.Errorf("Received %d", list.Head.Value)
+			}
+		})
+
+		t.Run("Tail value is 3", func(t *testing.T) {
+			if list.Tail.Value != 3 {
+				t.Errorf("Expected %d", 3)
+				t.Errorf("Received %d", list.Tail.Value)
+			}
+		})
+	})
+
+	t.Run("Get() element at index 1", func(t *testing.T) {
+		if node, _ := list.GetAt(1); node.Value != 2 {
+			t.Errorf("Expected %d", 2)
+			t.Errorf("Received %d", node.Value)
+		}
+	})
+
+	t.Run("RemoveAt()", func(t *testing.T) {
+		t.Run("Remove 0 index element returns the element", func(t *testing.T) {
+			value, _ := list.RemoveAt(0)
+
+			if value != 1 {
+				t.Errorf("Expected %d", 1)
+				t.Errorf("Received %d", value)
+			}
+		})
+
+		t.Run("Length is 2", func(t *testing.T) {
+			if list.Length != 2 {
+				t.Errorf("Expected %d", 2)
+				t.Errorf("Received %d", list.Length)
+			}
+		})
+	})
+
+	list.RemoveAt(0)
+	list.RemoveAt(0)
+
+	t.Run("Length is 0", func(t *testing.T) {
+		if list.Length != 0 {
+			t.Errorf("Expected %d", 0)
+			t.Errorf("Received %d", list.Length)
+		}
+	})
+
+	t.Run("Prepend()", func(t *testing.T) {
+		list.Prepend(1)
+		list.Prepend(2)
+		list.Prepend(3)
+
+		t.Run("Length is 3", func(t *testing.T) {
+			if list.Length != 3 {
+				t.Errorf("Expected %d", 3)
+				t.Errorf("Received %d", list.Length)
+			}
+		})
+
+		t.Run("Value of head is 3", func(t *testing.T) {
+			if list.Head.Value != 3 {
+				t.Errorf("Expected %d", 3)
+				t.Errorf("Received %d", list.Head.Value)
+			}
+		})
+
+		t.Run("Value of tail is 1", func(t *testing.T) {
+			if list.Tail.Value != 1 {
+				t.Errorf("Expected %d", 1)
+				t.Errorf("Received %d", list.Tail.Value)
+			}
+		})
+	})
+}
